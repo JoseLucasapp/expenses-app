d303484bcd986b1897ad13f91ce70cbeabfd0645
José Lucas <josluc2016@gmail.com>
2025-01-16 11:40:37 -0300

    feat: transaction list

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      ...transactions.map((tx) => Card(
+            child: Row(children: [
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
+                ),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.purple,
+                  width: 2,
+                )),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tx.amount.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(tx.title,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(DateFormat('d MMM y').format(tx.date),
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 100, 100, 100),
+                        fontSize: 15,
+                      )),
+                ],
+              ),
+            ]),
+          ))
+    ]);
+  }
+}
