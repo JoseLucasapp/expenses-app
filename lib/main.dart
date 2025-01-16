5ccb24bd37246220d974ade308781b7af25f154e
José Lucas <josluc2016@gmail.com>
2025-01-16 11:40:28 -0300

    fix: refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:expenses/components/transaction_list.dart';
+
 import './models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -56,49 +57,7 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          Column(children: [
-            ..._transactions.map((tx) => Card(
-                  child: Row(children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tx.amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          color: Colors.purple,
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(tx.title,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        Text(DateFormat('d MMM y').format(tx.date),
-                            style: TextStyle(
-                              color: const Color.fromARGB(255, 100, 100, 100),
-                              fontSize: 15,
-                            )),
-                      ],
-                    ),
-                  ]),
-                ))
-          ]),
+          TransactionList(_transactions),
           Card(
             elevation: 5,
             child: Column(children: [
