48e401c9d525e0ba176b2d29794dc4cb902dcee5
José Lucas <josluc2016@gmail.com>
2025-01-16 11:47:20 -0300

    fix: refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:expenses/components/transaction_list.dart';
-
-import './models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -13,24 +11,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final _transactions = [
-    Transaction(
-      id: '1',
-      title: 'Shirt',
-      amount: 110.0,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '2',
-      title: 'Shoes',
-      amount: 710.0,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,39 +37,7 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          TransactionList(_transactions),
-          Card(
-            elevation: 5,
-            child: Column(children: [
-              Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                          onPressed: () {
-                            print(titleController.text);
-                            print(valueController.text);
-                          },
-                          child: Text('Nova Transação')),
-                    ],
-                  )
-                ],
-              )
-            ]),
-          )
+          TransactionUser(),
         ]));
   }
 }
