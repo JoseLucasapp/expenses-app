8e39808d8834268bdee96269c49734573bf59cda
José Lucas <josluc2016@gmail.com>
2025-01-17 23:46:27 -0300

    fix: list view

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,50 +11,53 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: SingleChildScrollView(
-          child: Column(children: [
-            ...transactions.map((tx) => Card(
-                  child: Row(children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tx.amount.toStringAsFixed(2)}',
+        child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (context, index) {
+            final tx = transactions[index];
+
+            return Card(
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  )),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tx.amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(tx.title,
                         style: TextStyle(
-                          color: Colors.purple,
+                          color: Colors.black,
                           fontSize: 17,
                           fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(tx.title,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        Text(DateFormat('d MMM y').format(tx.date),
-                            style: TextStyle(
-                              color: const Color.fromARGB(255, 100, 100, 100),
-                              fontSize: 15,
-                            )),
-                      ],
-                    ),
-                  ]),
-                ))
-          ]),
+                        )),
+                    Text(DateFormat('d MMM y').format(tx.date),
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 100, 100, 100),
+                          fontSize: 15,
+                        )),
+                  ],
+                ),
+              ]),
+            );
+          },
         ));
   }
 }
