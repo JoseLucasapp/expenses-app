7308e6737c65e3d0221247b9c1e0332e1957fd79
José Lucas <josluc2016@gmail.com>
2025-01-21 22:08:48 -0300

    feat: image

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,53 +11,67 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView.builder(
-          itemCount: transactions.length,
-          itemBuilder: (context, index) {
-            final tx = transactions[index];
-
-            return Card(
-              child: Row(children: [
+        child: transactions.isEmpty
+            ? Column(children: [
+                Text(
+                  'Nenhuma transação cadastrada!',
+                  style: ThemeData.light().textTheme.titleLarge,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  )),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tx.amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(tx.title,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 17,
-                          fontWeight: FontWeight.bold,
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover))
+              ])
+            : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (context, index) {
+                  final tx = transactions[index];
+
+                  return Card(
+                    child: Row(children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
                         )),
-                    Text(DateFormat('d MMM y').format(tx.date),
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 100, 100, 100),
-                          fontSize: 15,
-                        )),
-                  ],
-                ),
-              ]),
-            );
-          },
-        ));
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${tx.amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            color: Colors.purple,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(tx.title,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          Text(DateFormat('d MMM y').format(tx.date),
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 100, 100, 100),
+                                fontSize: 15,
+                              )),
+                        ],
+                      ),
+                    ]),
+                  );
+                },
+              ));
   }
 }
